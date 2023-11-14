e3b14c9d888929d1d1b53b0f1b51e5fa0913899c
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-14 22:29:37 +0700

    3. Đổi trạng thái một công việc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const database = require("./config/database");
+const bodyParser = require("body-parser");
 
 // Routes Ver1
 const routesVer1 = require("./api/v1/routes/index.route");
@@ -10,6 +11,8 @@ database.connect();
 const app = express();
 const port = process.env.PORT;
 
+// parse application/json
+app.use(bodyParser.json());
 
 // Routes Ver1
 routesVer1(app);
