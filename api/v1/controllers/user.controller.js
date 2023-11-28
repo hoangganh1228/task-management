83251acda6da5942ecdc2e4bfe511b71b33b1cfe
Hoàng Anh. Nguyễn <hoanganh52521352@gmail.com>
2023-11-28 09:13:28 +0700

    6. Lưu những người tham gia vào task

--- api/v1/controllers/user.controller.js
+++ api/v1/controllers/user.controller.js
@@ -198,3 +198,14 @@ module.exports.detail = async (req, res) => {
     info: req.user
   });
 }
+
+// [POST] /api/v1/users/list
+module.exports.list = async (req, res) => { 
+  const users = await User.find({ deleted: false }).select("fullName email");
+
+  res.json({
+    code: 200,
+    message: "Thành công!",
+    users: users
+  });
+}
--- api/v1/middlewares/auth.middleware.js
+++ api/v1/middlewares/auth.middleware.js
@@ -1,31 +1,27 @@
 const User = require("../models/user.model");
 
 module.exports.requireAuth = async (req, res, next) => {
-    if(req.headers.authorization) {
-        const token = req.headers.authorization.split(" ")[1];
+  if(req.headers.authorization) {
+    const token = req.headers.authorization.split(" ")[1];
 
-        const user = await User.findOne({
-            token: token,
-            deleted: false
-        });
+    const user = await User.findOne({
+      token: token,
+      deleted: false
+    });
 
-        if(!user) {
-            res.json({
-                code: 403,
-                message: "Không có quyền truy cập!"
-            });
-        }
-        else {
-            req.user = user;
-            next();
-        }
-
-        
+    if(!user) {
+      res.json({
+        code: 403,
+        message: "Không có quyền truy cập!"
+      });
     } else {
-        res.json({
-            code: 403,
-            message: "Không có quyền truy cập!"
-        });
+      req.user = user;
+      next();
     }
-    
+  } else {
+    res.json({
+      code: 403,
+      message: "Không có quyền truy cập!"
+    });
+  }
 }
